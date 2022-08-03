7d035ea1ff6dcf58fd9ff0b29f822b616ead8769
Daniel Enriquez <72522628+daniel-enqz@users.noreply.github.com>
2022-08-02 20:09:57 -0500

    Update interface.rb

--- interface.rb
+++ interface.rb
@@ -4,7 +4,10 @@ human = Human.new("Jorge", 17)
 human_2 = Human.new("Daniel", 88)
 
 human.height = 1.76
+human.weight = 75
 
 puts human.name
 puts human.age
 puts human.height
+
+# Would I be able to print the weight as well 🤔????
